edf62e02264d1e3e9bd871f00db0ad1e4e368df4
Miriam Lipkovich <miri.lippa@yahoo.com>
2024-11-15 16:50:33 +0200

    finished

--- ex1.c
+++ ex1.c
@@ -10,14 +10,15 @@ Assignment: ex1
 int main() {
   
   // What bit
-  int num1, position, res =0, num2, res2 = 0, res3 = 0, answer = 0;
+  int num1, position, num2, res =0, res2 = 0, res3 = 0, answer = 0;
+  int const mask = 1;
   printf("What bit:\n");
   printf("Please enter a number:\n");
   scanf("%i", &num1);
   printf("Please enter a position:\n");
   scanf("%i", &position);
   res = num1 >> position;
-  answer = 1 & res;
+  answer = mask & res;
   printf("The bit in position %d of number %d is: %d", position, num1, answer);
   /*Scan two integers (representing number and a position)
   Print the bit in this position. */
@@ -28,7 +29,7 @@ int main() {
   scanf("%i", &num1);
   printf("Please enter a position:\n");
   scanf("%i", &position);
-  res = 1 << position;
+  res = mask << position;
   res2 = num1 | res;
   res3 = num1 & ~res;
   printf("Number with bit %d set to 1 is: %d\n",position, res2);
@@ -47,7 +48,7 @@ int main() {
   scanf("%i", &num1);
   printf("Please enter a position:\n");
   scanf("%i", &position);
-  res = 1 << position;
+  res = mask << position;
   answer = num1 ^ res;
   printf("Number with bit %d toggled: %d\n", position, answer);
   /*Scan two integers (representing number and a position)
@@ -58,21 +59,26 @@ int main() {
   printf("\nEven - Odd:\n");
   printf("Please enter a number: \n");
   scanf("%i", &num1);
-  res = 1 & num1;
+  res = mask & num1;
   printf("%d\n", res);
   /* Scan an integer
   If the number is even - print 1, else - print 0. */
   
   // 3, 5, 7, 11
+  int first = 3, second = 5, third = 7, fourth = 11;
+  int ans1, ans2, ans3, ans4;
   printf("\n3, 5, 7, 11:\n");
   printf("Please enter the first number (octal): \n");
   scanf("%o", &num1);
   printf("Please enter the second number (octal): \n");
   scanf("%o", &num2);
-  res = (num1 ^ num2) << 1;
+  res = num1 + num2;
   printf("The sum in hexadecimal: %X\n", res);
-  answer = (((res >> 3 & 1) >> 2 & 1) >> 2 & 1) >> 3 & 1;
-  printf("The 3,5,7,11 bits are: %d\n", answer);
+  ans1 = res >> first & mask;
+  ans2 = res >> second & mask;
+  ans3 = res >> third & mask;
+  ans4 = res >> fourth & mask;
+  printf("The 3,5,7,11 bits are: %d%d%d%d\n", ans1, ans2, ans3, ans4);
   /* Scan two integers in octal base
   sum them up and print the result in hexadecimal base
   Print only 4 bits, in positions: 3,5,7,11 in the result. */
